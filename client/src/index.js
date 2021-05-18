145700fb08c830094480821f30795ad356bc574e
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-17 22:15:20 -0500

    fixed imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import { BrowserRouter } from 'react-router-dom';
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
+ReactDOM.render(<App></App>, document.getElementById("root"));
+registerServiceWorker();
