5b50dec5c3545a4573d5fe47dac5c39c48ed89d8
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-17 22:12:34 -0500

    fixed issue with export default

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
+import React , { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Saved from "./Pages/Saved";
 import Search from "./Pages/Search";
 import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Jumbotron from "./Components/Jumbotron";
+// import Footer from "./Components/Footer";
 
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Jumbotron />
-        <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
 }
 
 export default App;
