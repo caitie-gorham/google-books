1dd5a4968b061cf149921f432cbe5f4e8cb929bb
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-17 22:10:56 -0500

    added search component

---
+++ client/src/Components/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function Input(props) {
+    return (
+        <div className="form-group">
+            <input className="form-control" {...props} />
+        </div>
+    )
+}
+export function SubmitButton(props) {
+    return (
+        <button {...props} className="btn submitBtn" style={{backgroundColor: "#2196f3", color: "white", marginBottom: "10px"}}>Search</button>
+    )
+}
