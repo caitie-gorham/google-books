37e711cd8662f420368ac867f9ca8063a4d1ce1a
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-17 22:07:30 -0500

    changed name of DB to match model

---
+++ controllers/booksController.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+      db.Book
+        .find(req.query)
+        .sort({ date: -1 })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    findOne: function(req, res) {
+      db.Book
+        .findOne({id: req.params.id})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    save: function(req, res) {
+      console.log("Trying to save a book")
+      console.log(req.body)
+      db.Book
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+      db.Book
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
+  };
